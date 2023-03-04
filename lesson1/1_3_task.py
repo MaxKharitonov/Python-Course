2921718f13a819e4b3ef3e1c53db34c859025710
Hikki <Hikki@komori.ru>
2023-03-04 22:57:23 +0300

    Modified solution for third task

--- lesson1/1_3_task.py
+++ lesson1/1_3_task.py
@@ -17,23 +17,19 @@ elif (ticket_number < 100000):
     print('Ошибка! Номер билета должен быть шестизначным!\n') # Вывод ошибки, если число меньше шестизначного.
     print('Введенное число меньше!\nЗапустите программу заново и введите корректное число.\nКонец программы...')
 else:
-    first_digit = ticket_number // 100000 # Получение первой цифры номера билета.
-    print('Первая цифра билета: {}'.format(first_digit))
-    second_digit = (ticket_number // 10000) % 10 # Получение второй цифры номера билета.
-    print('Вторая цифра билета: {}'.format(second_digit))
-    third_digit = (ticket_number // 1000) % 10 # Получение третьей цифры номера билета.
-    print('Третья цифра билета: {}'.format(third_digit))
-    fourth_digit = (ticket_number // 100) % 10 # Получение четвертой цифры номера билета.
-    print('Четвертая цифра билета: {}'.format(fourth_digit))
-    fifth_digit = (ticket_number // 10) % 10 # Получение пятой цифры номера билета.
-    print('Пятая цифра билета: {}'.format(fifth_digit))
-    sixth_digit = ticket_number % 10 # Получение шестой цифры номера билета.
-    print('Шестая цифра билета: {}\n'.format(sixth_digit))
-    first_three_digits_sum = first_digit + second_digit + third_digit # Нахождение суммы первых трех цифр номера билета.
-    print('Сумма первых трех цифр номера билета: {}'.format(first_three_digits_sum))
-    last_three_digits_sum = fourth_digit + fifth_digit + sixth_digit # Нахождение суммы последних трех цифр номера билета.
-    print('Сумма последних трех цифр номера билета: {}\n'.format(last_three_digits_sum))
+    last_three_digits_sum = 0
+    while ticket_number > 999: # Запуск цикла, суммирующего последние 3 цифры номера билета.
+        digit = ticket_number % 10
+        last_three_digits_sum += digit
+        # print('Last three digits sum is: {}'.format(last_three_digits_sum))
+        ticket_number //= 10
+    first_three_digits_sum = 0
+    while ticket_number > 0: # Запуск цикла, суммирующего первые 3 цифры номера билета.
+        digit = ticket_number % 10
+        first_three_digits_sum += digit
+        # print('First three digits sum is: {}'.format(first_three_digits_sum))
+        ticket_number //= 10
     if (first_three_digits_sum == last_three_digits_sum): # Проверка на равенство сумм первых и последних цифр номера билета.
-        print('Введенный номер билета {} является счастливым!\nКонец программы...'.format(ticket_number)) # Вывод результата
+        print('Введенный номер билета является счастливым!\nКонец программы...\n') # Вывод результата
     else:
-        print('Введенный номер билета {} не является счастливым!\nКонец программы...'.format(ticket_number))
+        print('Введенный номер билета не является счастливым!\nКонец программы...\n')
