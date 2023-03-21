6c2136f882ba7a6d3378e3d5cd775cbbe644f1c0
HikkiWork <fanatikl.p@bk.ru>
2023-03-21 17:36:01 +0300

    Added solution for first task

--- lesson4/4_1_task.py
+++ lesson4/4_1_task.py
@@ -2,3 +2,28 @@
 # Выдать без повторений в порядке возрастания все те числа, которые встречаются в обоих наборах.
 # Пользователь вводит 2 числа. n - кол-во элементов первого множества. m - кол-во элементов второго множества.
 # Затем пользователь вводит сами элементы множеств.
+
+def array_input(length):  # Функция заполнения массива
+    array = []
+    el_number = 1
+    for i in range(length):
+        input_array = int(input(f'\nВведите {el_number}-й элемент массива: '))
+        array.append(input_array)
+        el_number += 1
+        print(array)
+    return array
+
+
+def equal_elements(array1, array2):
+    array = []
+    for i in range(len(array1)):
+        for j in range(len(array2)):
+            if array1[i] == array2[j]:
+                number = array1[i]
+                array.append(number)
+                print(array)
+
+
+arr1 = array_input(int(input('\nВведите длину первого массива:')))
+arr2 = array_input(int(input('\nВведите длину второго массива:')))
+equal_elements(arr1, arr2)
